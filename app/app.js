ffacfc25aa1d975812de798a17856b033c479b1f
Vignesh Jayavel <vigneshjayavel@gmail.com>
2016-10-23 20:21:24 +0530

    remove crapcomments

--- app/app.js
+++ app/app.js
@@ -166,12 +166,9 @@
       //    type: status_change | priority_change | agent_change | group_change | note_addition
       //    data: STRING | NOTE_ID
       // }
-      // TODO
-      // var events = jQuery(this.activities).filter('.activity').each(function(i,a){
       var knownEventTypes = new Set(["agent", "status", "priority", "group"]);
       var that = this;
       jQuery('.conversation.activity').each(function(i,activity){
-        // TODO
         // Attach user, timestamp
         // jQuerycollection.each - takes 2 args ! 
         var $activity = jQuery(activity);
@@ -197,24 +194,7 @@
 
         jQuery.merge(that.events, eventsFromActivity);
         
-      })
-      // jQuery('.conversation').each(function(i,conversation){
-      //   if(conversation.has('.activity')) {
-      //     // general event except notes
-      //     var eventsFromActivity = ([].concat.apply([],jQuery(activity)
-      //       .find('.commentbox .details').text().strip().split('\\n')
-      //       .map(function(data) { 
-      //         return data.strip().split(',')
-      //       })
-      //     ))
-      //     .map(function(data) {
-      //       return data.replace("Set ","").split(' as ')
-      //     })  
-      //   } else if(conversation.has('.minimized')) {
-      //     // note event
-      //   }        
-      // })
-      // });
+      });
     },
     getSuccessParams: function(ticket){
       var arr =[]
